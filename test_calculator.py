76ff2b22f88e9f8e902f0be0cdad6530db114097
KoreyEnglert <englertk@oregonstate.edu>
2021-06-05 15:55:44 -0700

    fixed linking errors

--- test_calculator.py
+++ test_calculator.py
@@ -8,14 +8,11 @@ class TestCalculatorApp:
     def test_add(self):
         assert 2 == calculator.add(1, 1)
 
-
     def test_subtract(self):
         assert 0 == calculator.subtract(2, 2)
 
-
     def test_multiply(self):
         assert 4 == calculator.multiply(2, 2)
-
 
     def test_divide(self):
         assert 1 == calculator.divide(2, 2)
